65e7fdd285ce9a0d60255f349ad3aa73edcf3818
fanzheng10 <fanzheng1101@gmail.com>
2020-10-19 16:08:32 -0700

    Fan revision on Oct.19

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => (
       <VerticalTitle title='NeST' subtitle='Nested Systems in Tumors' />
       <VerticalButtonsContainer>
         <ButtonLink
-          text='View in NDEx'
+          text='View NeST in NDEx'
           link='http://www.ndexbio.org/?#/network/9a8f5326-aa6e-11ea-aaef-0ac135e8bacf'
           tooltip={true}
           tooltipPosition='top'
@@ -45,16 +45,37 @@ const IndexPage = () => (
           }
         />
         <ButtonLink
-          text='View in HiView'
-          link='http://hiview-test.ucsd.edu/274fcd6c-1adc-11ea-a741-0660b7976219?type=test&server=http://test.ndexbio.edu'
+          text='View NeST in HiView'
+          link='http://hiview.ucsd.edu/274fcd6c-1adc-11ea-a741-0660b7976219?type=test&server=http://test.ndexbio.edu'
           tooltip={true}
-          tooltipPosition='bottom'
+          tooltipPosition='top'
           tooltipText={
             <span>
               {'HiView is a tool for viewing large hierarchical networks. '}
               <a href='#hiview' style={{ color: 'rgb(0, 0, 0, 0.8)' }}>
                 Learn more
               </a>
+              {'.'}
+            </span>
+          }
+        />
+	<ButtonLink
+          text='View the protein network in NDEx'
+          link='http://www.ndexbio.org/?#/network/60112105-f853-11e9-bb65-0ac135e8bacf'
+          tooltip={true}
+          tooltipPosition='top'
+          tooltipText={
+            <span>
+              {'An integrated network used to assemble the NeST hierarchy.'}
+              <span
+                style={{
+                  display: 'inline-block',
+                }}
+              >
+                <a href='#ndex' style={{ color: 'rgb(0, 0, 0, 0.8)' }}>
+                  Learn more
+                </a>
+              </span>
               {'.'}
             </span>
           }
@@ -66,29 +87,27 @@ const IndexPage = () => (
       <h2>What is NeST?</h2>
       <p>
         {
-          'The NeST (Nested Systems in Tumors) is a hierarchical map of 371 protein systems recurrently mutated in 13 types of cancer in TCGA. The map recapitulates or expands canonical cancer pathways and identifies convergence of mutations on unexpected systems, many of which can be confirmed in validation cohorts. NeST systems provide a resource of mechanisms on which somatic mutations converge, as well as a resource for tumor genome interpretation, since many of the systems have substantial alteration frequencies with potential relevance to large numbers of patients.'
+          'The NeST (Nested Systems in Tumors) is a hierarchical map of 371 protein systems recurrently mutated in 13 cancer types cancer. The map recapitulates or expands canonical cancer systems including protein complexes and pathways, and identifies systems on which mutations unexpectedly converge. The NeST map provides a resource of cancer mechanisms under selection for somatic mutations.'
         }
       </p>
 
       <p>
-        {
-          'The NeST model was assembled by applying multiscale community detection ('
-        }
-        <LinkOut link='https://github.com/fanzheng10/CliXO-1.0'>code</LinkOut>
-        {') on a weighted protein interaction network ('}
-        <LinkOut link='http://www.ndexbio.org/#/network/60112105-f853-11e9-bb65-0ac135e8bacf'>
-          NDEx
+	{'The NeST model is assembled by first creating an integrated network ('}
+	<LinkOut link='http://www.ndexbio.org/#/network/60112105-f853-11e9-bb65-0ac135e8bacf'>
+         View in NDEx  
         </LinkOut>
         {'; '}
         <a href='/nest/files/IAS_CCMI.txt.gz' download='IAS_CCMI.txt.gz'>
-          TSV
-        </a>
-        {
-          ') integrating multiple categories of experimental evidence. A statistical model ('
-        }
+         Download
+        </a>	
+
+        {')  combining the evidence of interactions of five major data types: physical, mRNA co-expression, protein co-expression, sequence similarity, genetic co-dependence. Multiscale community detection ('}
+	<LinkOut link='https://github.com/fanzheng10/CliXO-1.0'>code</LinkOut>
+	{') was then used to detect protein systems of different scales, and '}
+	{'a statistical model ('}
         <LinkOut link='https://github.com/fanzheng10/HiSig'>code</LinkOut>
         {
-          ') was developed to assess the convergence of cancer mutations in protein systems. This workflow could serve as a template for hierarchical modeling of other diseases that are influenced by complex genetic alterations.'
+          ') was developed to assess the convergence of cancer mutations in protein systems. This workflow serves as a template for hierarchical modeling of other diseases influenced by complex genetic alterations.'
         }
       </p>
     </Panel>
@@ -100,22 +119,14 @@ const IndexPage = () => (
         <h2>What is NDEx?</h2>
       </a>
       <p>
-        {'NDEx, the Network Data Exchange ('}
-        <LinkOut link='http://ndexbio.org'>www.ndexbio.org</LinkOut>
-        {', Pratt et. al. 2017 '}
-        <LinkOut link='https://dx.doi.org/10.1158/0008-5472.CAN-17-0606'>
-          DOI: 10.1158/0008-5472.CAN-17-0606
+        <LinkOut link='http://ndexbio.org'>NDEx</LinkOut>
+	{' (the Network Data Exchange) is an online commons for biological networks where networks of many types and formats can be shared, accessed, and stored by users and applications ('}
+	<LinkOut link='https://dx.doi.org/10.1158/0008-5472.CAN-17-0606'>
+         Pratt et. al. 2017
         </LinkOut>
-        {
-          '), is an online commons for biological networks where networks of many types and formats can be shared, accessed, and stored by users and applications. NDEx is tightly integrated with Cytoscape ('
-        }
-        <LinkOut link='https://cytoscape.org/'>https://cytoscape.org/</LinkOut>
-        {'), and the '}
-        <LinkOut link='http://www.ndexbio.org/?#/network/9a8f5326-aa6e-11ea-aaef-0ac135e8bacf'>
-          NeST
-        </LinkOut>
-        {
-          ' network can be downloaded to Cytoscape by clicking the “Open in Cytoscape” button at the bottom-right of the web page.'
+	{'). NDEx is tightly integrated with '}
+        <LinkOut link='https://cytoscape.org/'>Cytoscape</LinkOut>
+        {'. The NeST model can be downloaded to Cytoscape by clicking the “Open in Cytoscape” button at the bottom-right of its NDEx page.'
         }
       </p>
       <h2>
@@ -123,12 +134,12 @@ const IndexPage = () => (
         <a id='hiview' />
       </h2>
       <p>
-        {'The HiView web application ('}
+        {'The '}
         <LinkOut link='http://hiview.ucsd.edu/'>
-          http://hiview.ucsd.edu/
+         HiView 
         </LinkOut>
         {
-          ') is an interactive visualization tool which supports exploration of the NeST and any similar hierarchical models. HiView allows the user to (1) interactively zoom across many scales in the model, from an expansive view of the entire hierarchy to more focused views of particular subsystems; (2) browse the network data supporting the inference of each subsystem; and (3) search genes and subsystems based on their names or metadata.'
+          ' web application is an interactive visualization tool which supports exploration of the NeST and any similar hierarchical models. HiView allows the user to (1) interactively zoom across many scales in the model, from an expansive view of the entire hierarchy to more focused views of particular subsystems; (2) browse the network data supporting the inference of each subsystem; and (3) search genes and subsystems based on their names or metadata.'
         }
       </p>
     </Panel>
